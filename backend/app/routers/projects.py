d56cb19ac140de0fe877703b1824ec0d284523c9
Artem Chuban <159516609+ArtemChuban@users.noreply.github.com>
2024-02-18 20:20:13 +0200

    OpenAPI responses (#36)

--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -1,50 +1,60 @@
-from typing import Annotated, List
+from typing import Annotated
 
 from fastapi import APIRouter, status, Depends, HTTPException
 
 from .dependences import get_user_by_init_data, get_project_by_id
-from app.schemas.Project import (
-    ProjectSchema,
-    ProjectCreateSchema,
-    ProjectUpdateSchema,
-)
+from app.schemas.Project import ProjectSchema, ProjectCreateSchema, ProjectUpdateSchema
+
 from app.schemas.User import UserSchema
 from app.services import project_service, user_service
+from .exceptions import Error
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
 
 
 @router.get(
     "/{project_id}",
+    response_model=ProjectSchema,
     status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def project_get(
     project: Annotated[ProjectSchema, Depends(get_project_by_id)],
-) -> ProjectSchema:
-    """Get information about the project"""
+):
     return project
 
 
 @router.get(
     "/by/user/id/{user_id}",
     status_code=status.HTTP_200_OK,
+    response_model=list[ProjectSchema],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def project_get_user(
     user: Annotated[UserSchema, Depends(get_user_by_init_data)],
-) -> List[ProjectSchema]:
-    """Get user's projects"""
+):
     return project_service.get_by_user_id(user.id)
 
 
 @router.post(
     path="/",
     status_code=status.HTTP_201_CREATED,
+    response_model=ProjectCreateSchema,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def project_add(
     project_create: ProjectCreateSchema,
     user: Annotated[UserSchema, Depends(get_user_by_init_data)],
-) -> None:
-    """Create a project"""
+):
     if not user_service.exist(project_create.user_id):
         raise HTTPException(status.HTTP_404_NOT_FOUND)
     return project_service.create(project_create)
@@ -54,13 +64,17 @@ def project_add(
 @router.put(
     path="/{project_id}",
     status_code=status.HTTP_200_OK,
+    response_model=int,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def update_project(
     project: Annotated[ProjectSchema, Depends(get_project_by_id)],
     project_update: ProjectUpdateSchema,
-) -> None:
-    """Change the project. Returns 1 if the change has occurred, otherwise returns 0"""
-    project_service.update(
+):
+    return project_service.update(
         project_id=project.id,
         project_update=project_update,
     )
@@ -69,9 +83,13 @@ def update_project(
 @router.delete(
     path="/{project_id}",
     status_code=status.HTTP_200_OK,
+    response_model=int,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def project_delete(
     project: Annotated[ProjectSchema, Depends(get_project_by_id)],
-) -> None:
-    """Delete a project. Returns 1 if deletion has occurred, otherwise returns 0"""
-    project_service.delete_by_id(project_id=project.id)
+):
+    return project_service.delete_by_id(project_id=project.id)
--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,29 +1,44 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status, HTTPException
-
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from .dependences import get_user_by_init_data
 from app.schemas.User import UserSchema, UserCreateSchema
 from app.services import user_service
+from .exceptions import Error
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get(
     path="/{user_id}",
+    response_model=UserSchema,
     status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_404_NOT_FOUND: {"model": Error},
+    },
 )
 def user_get(
     user: Annotated[UserSchema, Depends(get_user_by_init_data)],
-) -> UserSchema:
+):
     return user
 
 
 @router.post(
     path="/",
+    response_model=UserCreateSchema,
     status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Error},
+        status.HTTP_409_CONFLICT: {"model": Error},
+    },
 )
 def user_add(user: UserCreateSchema) -> None:
     if user_service.exist(user.id):
-        raise HTTPException(status.HTTP_409_CONFLICT)
+        return JSONResponse(
+            content=Error(message="User ID already exist").model_dump(),
+            status_code=status.HTTP_409_CONFLICT,
+            media_type="application/json",
+        )
     user_service.create(user)
