4990975bf5d9708cdbce5a048a8f82bbdbccecb6
Yury Shamanov <67365128+Eytes@users.noreply.github.com>
2024-02-20 16:58:40 +0200

    5 t0004 cicd (#6)
    * Create linter_flake8.yml
    
    * creating a configuration file for flake8
    
    * Create run_python_tests.yml
    
    * added TODO
    
    * fix linterfile name
    
    * added TODO
    
    * README initial
    
    * Удаление лишнего файла
    
    * launching the linter with any changes in the backend
    
    * automatic launch tests and linting when a pull request is sent to the main and dev branches
    
    * all configurations for printers are collected in one file
    
    * adding a black linter
    
    * adding a docker template
    
    * rename file with black linting
    
    * add schedule (every Sunday at midnight)
    
    * increase version of setup python action
    
    * deleted unnecessary properties
    
    * increase version of checkout action
    
    * temporarily disabling test runs
    
    * changing the trigger condition
    
    * temporarily disabling installing dependencies
    
    * fix
    
    * Added Python versions matrix
    
    * Add installation Python
    
    * Added a trigger when changing in the bot folder
    
    * Added a trigger when changing in the bot folder
    
    * combining linters
    
    * combining linters
    
    * increase version of python setup action
    
    * putting paths in an environment variable
    
    * rename
    
    * create frontend linting
    
    * created frontend autotests
    
    * using a ready-made linting action
    
    * fix node version
    
    * downgrade setup-node action
    
    * removing duplicate step
    
    * try to run
    
    * try to run 2
    
    * fix CI github does not start
    
    * disabling the cache
    
    * install linters
    
    * install linters
    
    * configuration for eslint
    
    * delete TODO. The authors do not support the ability to add configuration to packages.json
    
    * install eslint-plugin-react
    
    * I took out the file extensions separately
    
    * try to find error
    
    * try to fix error
    
    * try to fix error
    
    * try to fix error
    
    * try to fix error
    
    * change ECMA
    
    * transferring flake8 settings to the CI configuration
    
    * fix
    
    * fix
    
    * try to fix eslint
    
    * try to fix eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    * try to run eslint
    
    ---------
    
    Co-authored-by: ArtemChuban <ArtemChuban@yandex.ru>

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,10 +5,16 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "prettier",
+    "react"
+  ],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
