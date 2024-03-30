2d94b55cabbdfd863e4444a02f67a84a2718e30a
Artem Chuban <ArtemChuban@yandex.ru>
2024-03-30 15:07:05 +0200

    Telegram class (#111)
    * Telegram class
    
    * comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,18 @@
 import { useEffect } from "react";
 import { AppRoute } from "./app/routes/AppRoute";
-import { tg } from "./app/tg";
-import { IUser } from "./pages/Profile/Profile";
+import { telegram } from "./app/tg";
 import { useAppDispatch } from "./app/hooks/useAppDispatch";
 import { getUser } from "./app/store/slice/UserSlice/userApi";
 import { MenuProvider } from "./app/providers/MenuProvider";
 import { ResposeProvider } from "./app/providers/ResponseProvider";
 
 function App() {
-  const tgUser: IUser = tg.initDataUnsafe.user; // tgUser - это данные о пользователе, которые беруться с телеграма
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    tg.ready();
-    tg.expand();
-    tg.enableClosingConfirmation();
-    if (tgUser && tgUser.id) {
-      dispatch(getUser(tgUser.id)); // Если tgUser есть - используем его данные, если нет - используются моковые данные
-    }
-    // Моковые данные
-    else {
-      dispatch(getUser("0"));
-    }
-  }, [tgUser]);
+    telegram.windowInitiate();
+    dispatch(getUser(telegram.user.id));
+  }, []);
   return (
     <>
       <ResposeProvider>
--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { useAppSelector } from "src/app/hooks/useAppSelector";
-import { tg } from "src/app/tg";
-export interface IUser {
-  id?: string;
-  login?: string;
-}
 
 const Profile: React.FC = () => {
-  const tgUser: IUser = tg.initDataUnsafe.user;
   const id = useAppSelector((state) => state.user.user?.id);
 
   return (
@@ -31,7 +25,7 @@ const Profile: React.FC = () => {
     //     </li>
     //   </ul>
     // </div>
-    <>{tgUser ? tgUser?.id : id}</>
+    <>{id}</>
   );
 };
 
