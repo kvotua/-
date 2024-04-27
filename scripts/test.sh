75bc3b775f4711cd115a28730873ece714104973
Artem Chuban <ArtemChuban@yandex.ru>
2024-04-27 11:27:43 +0200

    env file for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,6 @@ set -x
 
 VERSION=${1:-3.11}
 
-docker compose build backend --build-arg VERSION=$VERSION -q
-docker compose -f docker-compose.yml -f docker-compose.test.yml up backend --force-recreate --always-recreate-deps --abort-on-container-exit --no-log-prefix || break
-docker compose down -v
+docker compose --env-file .env.test build backend --build-arg VERSION=$VERSION -q
+docker compose --env-file .env.test -f docker-compose.yml -f docker-compose.test.yml up backend --force-recreate --always-recreate-deps --abort-on-container-exit --no-log-prefix || break
+docker compose --env-file .env.test down -v
