45c2d37f8336b85ea93b4eb5dcf9f1907f369356
Artem Chuban <ArtemChuban@yandex.ru>
2024-04-27 11:20:08 +0200

    move tests into docker container

--- backend/tests/setup.py
+++ backend/tests/setup.py
@@ -1,8 +1,5 @@
-from moto import mock_aws
-
 from app.main import app
 
 from .api import ClientApi
 
-mock_aws().start()
 client = ClientApi(app)
---
+++ scripts/test.sh
@@ -0,0 +1,7 @@
+set -x
+
+VERSION=${1:-3.11}
+
+docker compose build backend --build-arg VERSION=$VERSION -q
+docker compose -f docker-compose.yml -f docker-compose.test.yml up backend --force-recreate --always-recreate-deps --abort-on-container-exit --no-log-prefix || break
+docker compose down -v
