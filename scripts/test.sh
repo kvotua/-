2713ba7cc6bb8d2bf560dd0ee2880e58b4d0f79b
Artem Chuban <ArtemChuban@yandex.ru>
2024-04-27 11:33:18 +0200

    force exit code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,10 @@
 set -x
 
 VERSION=${1:-3.11}
+COMMAND="docker compose --env-file .env.test"
 
-docker compose --env-file .env.test build backend --build-arg VERSION=$VERSION -q
-docker compose --env-file .env.test -f docker-compose.yml -f docker-compose.test.yml up backend --force-recreate --always-recreate-deps --abort-on-container-exit --no-log-prefix || break
-docker compose --env-file .env.test down -v
+$COMMAND build backend --build-arg VERSION=$VERSION -q
+$COMMAND -f docker-compose.yml -f docker-compose.test.yml up backend --force-recreate --always-recreate-deps --abort-on-container-exit --no-log-prefix
+CODE=$?
+$COMMAND down -v
+exit $CODE
